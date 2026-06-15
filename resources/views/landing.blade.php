7ed130859e98b8b1d90dda0bf10598d1bcb7672e
vallreey <revalsapertama@gmail.com>
2026-06-15 20:30:02 +0700

    update landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -253,12 +253,6 @@
             OreNoAiba
         </a>
 
-        <div class="nav-menu">
-            <a href="#beranda">Beranda</a>
-            <a href="#keunggulan">Keunggulan</a>
-            <a href="#fitur">Fitur</a>
-            <a href="#tentang">Tentang</a>
-        </div>
 
         <div class="nav-actions">
             <a href="{{ route('login') }}" class="login-link">Masuk</a>
